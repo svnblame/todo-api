7897767bf12ed224c1c46e460679f47f75c7403f
Gene Kelley <gene@bizflowdesigns.com>
2015-11-12 14:32:38 -0600

    Refactored using underscore.

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
-
 var bodyParser = require('body-parser');
+var _ = require('underscore');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -19,17 +19,11 @@ app.get('/todos', function(req, res) {
     res.json(todos);
 });
 
+// GET /todos/:id
 app.get('/todos/:id', function(req, res) {
     var todoId = parseInt(req.params.id, 10);
 
-    // Iterate over todos array and find the match
-    var matchedTodo;
-
-    todos.forEach(function(todo) {
-        if (todoId === todo.id) {
-            matchedTodo = todo;
-        }
-    });
+    var matchedTodo = _.findWhere(todos, {id: todoId});
 
     if (matchedTodo) {
         res.json(matchedTodo);
@@ -40,6 +34,10 @@ app.get('/todos/:id', function(req, res) {
 
 app.post('/todos', function(req, res) {
     var body = req.body;
+    
+    if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+        return res.status(400).send();
+    }
 
     // add id field
     body.id = todoNextId++;
