18d814e96dbd8be9a68558b78e2ebfc34d2905d2
Gene Kelley <gene@phpcodehead.com>
2015-11-12 11:32:33 -0600

    Added POST /todos

--- server.js
+++ server.js
@@ -1,28 +1,15 @@
 var express = require('express');
+
+var bodyParser = require('body-parser');
+
 var app = express();
 var PORT = process.env.PORT || 3000;
 
 // ToDo list (collection)
-var todos = [
-    // todo items (model(s))
-    {
-        id: 1,
-        description: 'Meet Mom for dinner',
-        completed: false
-    }, {
-        id: 2,
-        description: 'Go to Cherry Valley Mall',
-        completed: false
-    }, {
-        id: 3,
-        description: 'Get new dumb phones',
-        completed: true
-    }, {
-        id: 4,
-        description: 'Go shopping at Meijers',
-        completed: true
-    }
-];
+var todos = [];
+var todoNextId = 1;
+
+app.use(bodyParser.json());
 
 app.get('/', function(req, res) {
     res.send('Todo API Root');
@@ -49,6 +36,18 @@ app.get('/todos/:id', function(req, res) {
     } else {
         res.status(404).send('Match not found');
     }
+});
+
+app.post('/todos', function(req, res) {
+    var body = req.body;
+
+    // add id field
+    body.id = todoNextId++;
+
+    // push body into array
+    todos.push(body);
+
+    res.json(body);
 });
 
 app.listen(PORT, function() {
