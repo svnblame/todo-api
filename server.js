d5e08d0825378d28152ddb87ed2b5be7828458fe
Gene Kelley <gene@bizflowdesigns.com>
2015-11-19 15:19:26 -0600

    Converted DELETE /todos/:id to sequelize

--- server.js
+++ server.js
@@ -35,7 +35,9 @@ app.get('/todos', function(req, res) {
         };
     }
 
-    db.todo.findAll({where: where}).then(function(todos) {
+    db.todo.findAll({
+        where: where
+    }).then(function(todos) {
         res.json(todos);
     }, function() {
         res.status(500).send();
@@ -75,18 +77,21 @@ app.post('/todos', function(req, res) {
 // DELETE /todos/:id
 app.delete('/todos/:id', function(req, res) {
     var todoId = parseInt(req.params.id);
-    var matchedTodo = _.findWhere(todos, {
-        id: todoId
+    db.todo.destroy({
+        where: {
+            id: todoId
+        }
+    }).then(function(rowsDeleted) {
+        if (rowsDeleted === 0) {
+            res.status(404).json({
+                error: 'No todo with that id found'
+            })
+        } else {
+            res.status(204).send();
+        }
+    }, function() {
+        res.status(500).send();
     });
-
-    if (!matchedTodo) {
-        res.status(404).json({
-            "error": "no todo found with that id"
-        });
-    } else {
-        todos = _.without(todos, matchedTodo);
-        res.json(matchedTodo);
-    }
 });
 
 // PUT /todos/:id
