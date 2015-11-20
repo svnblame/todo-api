ec41ed56a0bc84ec1d31e468223e925185a2fd5a
Gene Kelley <gene@bizflowdesigns.com>
2015-11-20 14:31:12 -0600

    Added sequelize hook that converts user.email to lowercase on validation checks

--- models/user.js
+++ models/user.js
@@ -15,5 +15,14 @@ module.exports = function(sequelize, DataTypes) {
 				len: [7, 100]
 			}
 		}
+	}, {
+		hooks: {
+			beforeValidate: function(user, options) {
+				// convert user.email to lowercase if it is a string
+				if (typeof user.email === 'string') {
+					user.email = user.email.toLowerCase();
+				}
+			}
+		}
 	});
 }
