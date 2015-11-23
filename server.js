daf637392caae0d9516ae8d64b92ae1fcc6ccef8
Gene Kelley <gene@bizflowdesigns.com>
2015-11-23 12:02:36 -0600

    Converted CRUD requests to use currently logged in user's associated todo items only.

--- server.js
+++ server.js
@@ -19,9 +19,12 @@ app.get('/', function(req, res) {
     res.send('Todo API Root');
 });
 
+// GET /todos
 app.get('/todos', middleware.requireAuthentication, function(req, res) {
     var query = req.query;
-    var where = {};
+    var where = {
+        userId: req.user.get('id')
+    };
 
     // If 'completed' query parameter was provided, return items filtered on provided value 
     // (true or false)
@@ -51,7 +54,12 @@ app.get('/todos', middleware.requireAuthentication, function(req, res) {
 app.get('/todos/:id', middleware.requireAuthentication, function(req, res) {
     var todoId = parseInt(req.params.id, 10);
 
-    db.todo.findById(todoId).then(function(todo) {
+    db.todo.findOne({
+        where: {
+            id: todoId,
+            userId: req.user.get('id')
+        }
+    }).then(function(todo) {
         if (!!todo) {
             res.json(todo.toJSON());
         } else {
@@ -86,7 +94,8 @@ app.delete('/todos/:id', middleware.requireAuthentication, function(req, res) {
     var todoId = parseInt(req.params.id);
     db.todo.destroy({
         where: {
-            id: todoId
+            id: todoId,
+            userId: req.user.get('id')
         }
     }).then(function(rowsDeleted) {
         if (rowsDeleted === 0) {
@@ -116,7 +125,12 @@ app.put('/todos/:id', middleware.requireAuthentication, function(req, res) {
     }
 
     // Update
-    db.todo.findById(todoId).then(function(todo) {
+    db.todo.findOne({
+        where: {
+            id: todoId,
+            userId: req.user.get('id')
+        }
+    }).then(function(todo) {
         if (todo) {
             todo.update(attributes).then(function(todo) {
                 res.json(todo.toJSON());
